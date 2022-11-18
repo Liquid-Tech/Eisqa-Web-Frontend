e8b937e9af24cfb3101350bd548260cba58320f2
Ch Tayyab <80338648+ChTayyab786@users.noreply.github.com>
2022-11-19 02:33:06 +0500

    Find Job

--- src/Components/Cards/GigCard.jsx
+++ src/Components/Cards/GigCard.jsx
@@ -3,14 +3,17 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  Button,
   Typography,
   CardHeader,
   Avatar,
+  Box,
+  IconButton,
 } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function GigCard({ data }) {
-  
   return (
     <Card sx={{ maxWidth: 345, boxShadow: "0px 32px 70px 0px #0000000A" }}>
       <CardMedia
@@ -20,25 +23,61 @@ export default function GigCard({ data }) {
         image={data.gigImg}
       />
       <CardHeader
-        sx={{ px: 1 }}
+        sx={{
+          px: 1,
+          ".MuiCardHeader-avatar": {
+            marginRight: "10px",
+          },
+          ".MuiCardHeader-title": {
+            fontFamily: "Poppins !important",
+          },
+          ".MuiCardHeader-subheader": {
+            fontSize: "12px",
+            fontFamily: "Poppins !important",
+          },
+        }}
+        className="poppins"
         avatar={<Avatar src={data.userImage} alt={data.userName} />}
         action={
-          <Typography variant="body2" color="text.secondary" px={2}>
-            {data.rating}
-          </Typography>
+          <Box sx={{ display: "flex", alignItems: "baseline", m: 1 }}>
+            <Typography
+              sx={{ fontSize: "12px", color: "#FFDD00", fontWeight: "500" }}
+              className="poppins"
+            >
+              <StarIcon fontSize="inherit" />
+              {data.rating}
+            </Typography>
+            &nbsp;
+            <Typography
+              sx={{ fontSize: "11px" }}
+              color="text.secondary"
+              className="poppins"
+            >
+              {`(${data.comments})`}
+            </Typography>
+          </Box>
         }
         title={data.userName}
         subheader={data.level}
       />
       <CardContent sx={{ p: 1, pt: 0 }}>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" className="poppins">
           {data.gigDesc}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Typography variant="body2" color="text.secondary">
-          {data.price}$/starting
+      <CardActions sx={{ borderTop: "1px solid #707070", p: 1 }}>
+        <Typography variant="body2" color="text.secondary" className="poppins">
+          <span style={{ color: "#1EBF73" }}>{data.price}$</span> /starting
         </Typography>
+        <IconButton aria-label="menu" sx={{ ml: "auto", p: "4px" }}>
+          <MenuIcon fontSize="small" />
+        </IconButton>
+        <IconButton
+          aria-label="add to favorites"
+          sx={{ ml: "0px !important", p: "4px" }}
+        >
+          <FavoriteIcon fontSize="small" />
+        </IconButton>
       </CardActions>
     </Card>
   );
--- src/Pages/Seller/FindJob/FindJob.jsx
+++ src/Pages/Seller/FindJob/FindJob.jsx
@@ -1,35 +1,169 @@
+import { useState } from "react";
 import {
-  Divider,
-  Typography,
-  Card,
-  CardActionArea,
-  Paper,
   Grid,
   Box,
+  Button,
+  TextField,
+  InputAdornment,
+  ButtonBase,
+  Typography,
+  IconButton,
+  Tooltip,
+  styled,
+  tooltipClasses,
+  Divider,
 } from "@mui/material";
-import dayjs from "dayjs";
+import search_icon from "../../../assets/images/search-Icon-1.svg";
+import swap_icon from "../../../assets/images/swap.svg";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import SouthIcon from "@mui/icons-material/South";
+import NorthIcon from "@mui/icons-material/North";
 import "./styles.scss";
+import JobCard from "../../../Components/Cards/JobCard";
+
+const LightTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.green.main,
+    color: "#fff",
+    boxShadow: theme.shadows[1],
+    fontSize: 12,
+    fontWeight: 500,
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    backgroundColor: "#fff",
+    color: theme.palette.green.main,
+    fontSize: 22,
+  },
+}));
+
 const FindJob = () => {
+  // state
+  const [toggleBtn, setToggleBtn] = useState("search");
+
+  // toggle between search and saved jobs
+  const handleToggleBtn = (type) => {
+    setToggleBtn(type);
+  };
+
   return (
-    <Box className="Dashboard">
-      <Grid container alignItems="stretch" columnSpacing={2} rowSpacing={2}>
-        <Grid item xs={12}>
-          <Divider textAlign="left">
-            <Typography variant="h5" color="primary">
-              Find Job
-            </Typography>
-          </Divider>
+    <Box className="find-job-page">
+      <Grid
+        container
+        sx={{ p: { md: 5, xs: 3 }, mt: { md: 3, xs: 0 } }}
+        rowSpacing={2}
+        columnSpacing={{ md: 5, xs: 1 }}
+      >
+        <Grid item xs={6} className="d-flex justify-content-end">
+          <Button
+            variant={toggleBtn === "search" ? "contained" : "outlined"}
+            className={
+              toggleBtn === "search" ? "active-btn toggle-btn" : "toggle-btn"
+            }
+            onClick={() => handleToggleBtn("search")}
+          >
+            Search
+          </Button>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Box>
-            <CardActionArea>
-              <Paper component={Card} elevation={3} className="p-3">
-                <Typography variant="body2" color="primary">
-                  {dayjs().format("MMM DD, YYYY")}
-                </Typography>
-              </Paper>
-            </CardActionArea>
+        <Grid item xs={6} className="d-flex justify-content-start">
+          <Button
+            variant={toggleBtn === "savedJob" ? "contained" : "outlined"}
+            className={
+              toggleBtn === "savedJob" ? "active-btn toggle-btn" : "toggle-btn"
+            }
+            onClick={() => handleToggleBtn("savedJob")}
+          >
+            Saved Jobs
+          </Button>
+        </Grid>
+        <Grid item xs={12} className="search-box">
+          <TextField
+            fullWidth
+            variant="outlined"
+            size="small"
+            placeholder="Search"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <img src={search_icon} alt="" width="50%" />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment
+                  component={ButtonBase}
+                  sx={{
+                    backgroundColor: "#1ebf73",
+                    color: "#fff",
+                    padding: "22.5px 5%",
+                    borderTopLeftRadius: 0,
+                    borderTopRightRadius: "7px",
+                    borderBottomLeftRadius: 0,
+                    borderBottomRightRadius: "7px",
+                  }}
+                  onClick={() => console.log("Hello ??")}
+                >
+                  Search
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6" className="heading">
+            <span>12,456 </span>jobs found
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Box className="filters">
+            <Button endIcon={<ExpandMoreIcon color="green" />}>Filters</Button>
+            <Box className="sort-by">
+              <Typography variant="caption" className="text">
+                Sort By
+              </Typography>
+              <LightTooltip
+                title={
+                  <Box className="d-flex flex-column p-1">
+                    <Box py={1}>
+                      <NorthIcon fontSize="small" /> Sort Descending
+                    </Box>
+                    <Divider
+                      sx={{
+                        bgcolor: "#fff",
+                        height: 3,
+                      }}
+                    />
+                    <Box py={1}>
+                      <SouthIcon fontSize="small" /> Sort Ascending
+                    </Box>
+                  </Box>
+                }
+                arrow
+                placement="bottom-end"
+              >
+                <IconButton aria-label="swap_icon">
+                  <img src={swap_icon} alt="swap_icon" width="25px" />
+                </IconButton>
+              </LightTooltip>
+            </Box>
           </Box>
+          {JobData.map((item, idx) => (
+            <JobCard key={idx} data={item} />
+          ))}
+        </Grid>
+        <Grid item xs={12} className="text-center">
+          <Button
+            variant="outlined"
+            className="poppins fw-semibold"
+            sx={{
+              textTransform: "none",
+              color: "rgba(112, 112, 112, 0.65)",
+              borderColor: "rgba(112, 112, 112, 0.65)",
+              borderRadius: "8px",
+            }}
+          >
+            View More
+          </Button>
         </Grid>
       </Grid>
     </Box>
@@ -37,3 +171,27 @@ const FindJob = () => {
 };
 
 export default FindJob;
+
+const JobData = [
+  {
+    title: "Minimalistic Website & App Design/Development",
+    tags: "Fixed price-Entry level-Est. Budget :25$-Posted 4 minutes ago",
+    desc: "We want an extremely minimalistic and easy to use website and mobile app for an online B2B ordering app. This project is for both design and dev, but if you're great at either one of them, feel free to apply as well with your portfolio.",
+    skills: ["Design", "Mobile Design", "Design", "Mobile Design", "Graphics"],
+    proposals: "Less than 5",
+  },
+  {
+    title: "App Design/Development",
+    tags: "Fixed price-Entry level-Est. Budget :25$-Posted 4 minutes ago",
+    desc: "We want an extremely minimalistic and easy to use website and mobile app for an online B2B ordering app. This project is for both design and dev, but if you're great at either one of them, feel free to apply as well with your portfolio.",
+    skills: ["Design", "Mobile Design", "Design", "Mobile Design", "Graphics"],
+    proposals: "Less than 5",
+  },
+  {
+    title: "Minimalistic Website Development",
+    tags: "Fixed price-Entry level-Est. Budget :25$-Posted 4 minutes ago",
+    desc: "We want an extremely minimalistic and easy to use website and mobile app for an online B2B ordering app. This project is for both design and dev, but if you're great at either one of them, feel free to apply as well with your portfolio.",
+    skills: ["Design", "Mobile Design", "Design", "Mobile Design", "Graphics"],
+    proposals: "Less than 5",
+  },
+];
